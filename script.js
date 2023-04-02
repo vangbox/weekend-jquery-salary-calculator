217b388e7a760b5ee8e5c4cad91195cb4613c70c
Vang Vang <vang2code@gmail.com>
2023-04-02 13:31:15 -0500

    working on the calculation of annual salary

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function onSubmit(event){
     let jobTitle = $('#job-title').val();
     let annualSalary = $('#annual-salary').val();
 
-    $('tbody').append(`
+    $('#tbody').append(`
         <tr>
             <td>${firstName}</td>
             <td>${lastName}</td>
@@ -27,10 +27,28 @@ function onSubmit(event){
             <td>
                 <button class='delete-button'>🗑️</button
             </td>
-            
+
         </tr>
     `)
+
+    
+    // let annualSalaryNum = Number($('#annual-salary').text());
+    // let totalAnnualSalaryNum = Number($('#total-annual-salary').text());
+    // let totalSum = annualSalaryNum + totalAnnualSalaryNum;
+    // $('#total-annual-salary').text(totalSum);
+
+    // let annualSalaryNum = $('#annual-salary');
+    // let totalAnnualSalaryNum = $('#total-annual-salary');
+    // let annualSalarySum = Number(annualSalaryNum.text()) + Number(totalAnnualSalaryNum.text());
+    // $('#total-annual-salary').text(annualSalarySum);
+    let start = 0;
+    let num = Number($('#total-annual-salary').text()) + Number($('#annual-salary').text());
+    let total = start + num;
+    $('#total-annual-salary').text(total);
+    
+
 };
+
 
 function onDelete(){
     $(this).closest('tr').remove();
