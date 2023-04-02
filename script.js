98354866466f53aba32adf7af7b54d66d52da8a2
Vang Vang <vang2code@gmail.com>
2023-04-02 15:12:16 -0500

    got the monthly function and red box working

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function onReady(){
     //console.log('Tester'); - it works!
 
     $('#submit-button').on('click', onSubmit);
-
+   
     $('#tbody').on('click', '.delete-button', onDelete);
 }
 
@@ -31,7 +31,29 @@ function onSubmit(event){
         </tr>
     `)
 
-    
+
+    let totalAnnualSalaryNum = Number($('#total-annual-salary').text());
+    let annualSalaryNum = annualSalary/12 +totalAnnualSalaryNum;
+    // console.log(annualSalaryNum);
+    $('#total-annual-salary').text(annualSalaryNum);
+
+   
+    if(annualSalaryNum > 20000){
+    //console.log("test red"); test - it works!
+    $('#total-annual-salary').css('background', 'red');
+    }   
+  
+};
+
+
+
+    // $(function(){
+    // // let start = 0;
+    // let totalAnnualSalaryNum = Number($('#total-annual-salary').text());
+    // let annualSalaryNum = Number($('#annual-salary').text());
+    // totalAnnualSalaryNum += annualSalaryNum;
+    // $('#total-annual-salary').text(totalAnnualSalaryNum);
+    // })
     // let annualSalaryNum = Number($('#annual-salary').text());
     // let totalAnnualSalaryNum = Number($('#total-annual-salary').text());
     // let totalSum = annualSalaryNum + totalAnnualSalaryNum;
@@ -41,18 +63,35 @@ function onSubmit(event){
     // let totalAnnualSalaryNum = $('#total-annual-salary');
     // let annualSalarySum = Number(annualSalaryNum.text()) + Number(totalAnnualSalaryNum.text());
     // $('#total-annual-salary').text(annualSalarySum);
-    let start = 0;
-    let num = Number($('#total-annual-salary').text()) + Number($('#annual-salary').text());
-    let total = start + num;
-    $('#total-annual-salary').text(total);
+    // let start = 0;
+
+    // let start = 0;
+    // let num = Number($('#annual-salary').text());
+    // num + Number($('#total-annual-salary').text());
+    // start += num;
+    // Number($('#total-annual-salary').text(start));
+    // console.log("test");
     
-    if($('#total-annual-salary' > Number(20000).text())){
-    $('#total-annual-salary').css('background', 'red');
-    }   
-};
+  
 
+    // let annualTable = Number($('#annual-table').text);
 
+    // for (let i = 0; i < annualTable.length; i++) {
+    //     Number(annualTable[i].innterHTML);
+    // }
+    
+    // console.log(annualTable);
 
+    // $(function(){
+    //         let annualSalary = Number($('#total-annual-salary').text());
+    //         let totalAnnual = 0;
+    //         for(let i=0;i<annualSalary.length;i++){
+    //             if(Number(annualSalary[i].text()))
+    //                 totalAnnual += Number(annualSalary[i].text());
+    //         }
+    //         $('#total-annual-salary').text = totalAnnual;
+    //         Number($('#total-annual-salary').text(totalAnnual));
+    // })
 
 function onDelete(){
     $(this).closest('tr').remove();
